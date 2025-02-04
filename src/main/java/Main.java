24f8cdd2781f96d146c625bfddf929d102a9c634
Anna Veretencheva <averetencheva@inno.tech>
2025-02-04 21:25:00 +0300

    created new branch, homework completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        System.out.println("Введите число");
+        int number1= new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int number2= new Scanner(System.in).nextInt();
+        System.out.println("сумма чисел равна: "+ (number1+number2));
+        System.out.println("разность чисел равна: "+ (number1-number2));
+        System.out.println("произведение чисел равно: "+ (number1*number2));
+        System.out.println("частное чисел равно: "+ ((double)number1/number2));
     }
 }
