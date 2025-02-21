50c4800eaad9d7f72656753312d3e7da4b250fb2
Anna Veretencheva <averetencheva@inno.tech>
2025-02-21 19:58:58 +0300

    added scanner functions to determine the existence of a file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isFile = file.isFile();
+
+            if (!fileExists) {
+                System.out.println("Файл не существует.");
+                continue;
+            }
+
+            if (!isFile) {
+                System.out.println("Указанный путь ведет к папке, а не к файлу.");
+                continue;
+            }
+
+            counter++;
+            System.out.println("Путь указан верно. Это файл номер " + counter);
+        }
     }
 }
